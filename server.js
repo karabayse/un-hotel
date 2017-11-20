4aac2e20a07532730bffaf1764e2da5f417c45a7
Kara Bayse <karabayse@gmail.com>
2017-11-20 16:17:39 -0600

    server up again after new files added

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // requires
 var express = require('express');
 var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // uses
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  console.log('base url hit');
+  res.sendFile(path.resolve('public/views/index.html'));
+});
 
 // globals
 var port = process.env.PORT || 3333;
